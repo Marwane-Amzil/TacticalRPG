9e75d017bcb2af324bbf9f915018525f856e5a08
Amzil Marwane <63516970+sardinefroidedu4@users.noreply.github.com>
2022-11-17 13:47:37 +0100

    "Adding the constants file, that constains all the variables needed and used in a lot of places in the code. "

---
+++ include/Constants.hpp
@@ -0,0 +1,24 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+// The goal of this file is to create local varialbes that are going to be used in the whole project 
+
+
+// define your own namespace to hold constants
+
+
+namespace constants_GRID {
+    constexpr int ROWS = 20;
+    constexpr int COLUMNS = 20;
+}
+
+namespace constants_Mage {
+    constexpr int HP = 0;
+    constexpr int MAGIC_POWER = 0;
+}
+
+// ... other related constants
+
+
+
+#endif

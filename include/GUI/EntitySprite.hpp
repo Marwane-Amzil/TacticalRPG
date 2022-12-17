199be20970c35c93f002e70194a9a577c6860538
HoussamCzn <105058712+HoussamCzn@users.noreply.github.com>
2022-12-17 10:29:42 +0100

    feat: complete rework of EntitySprite
    EntitySprite is now basically a Sprite class. Removed the inheritance from sf::Sprite. It has the specifity to bring an entity with it, thus, it will be responsible for the deletion of its entity.

--- include/GUI/EntitySprite.hpp
+++ include/GUI/EntitySprite.hpp
@@ -3,26 +3,64 @@
 #ifndef __ENTITY_SPRITE_H__
 #define __ENTITY_SPRITE_H__
 
-#include <SFML/Graphics/Sprite.hpp>
-#include <string>
+#include <SFML/Graphics/Drawable.hpp> // sf::Drawable
+#include <SFML/Graphics/Transformable.hpp> // sf::Transformable
+#include <SFML/Graphics/Vertex.hpp> // sf::Vertex
+#include <SFML/Graphics/Rect.hpp> // sf::IntRect, sf::FloatRect
+#include <array> // std::array
 
 class Entity;
 
+namespace sf
+{
+	class Texture;
+}
+
 namespace gui
 {
-	class EntitySprite : public sf::Sprite
+	class Animation;
+
+	class EntitySprite : public sf::Drawable, public sf::Transformable
 	{
 	public:
 
 		explicit EntitySprite(Entity* entity = nullptr);
 
-		inline virtual ~EntitySprite() noexcept = default;
-
 		const Entity* getEntity() const;
+
+		Entity* getEntity();
+
+		const sf::Texture* getTexture() const;
+
+		const sf::IntRect& getTextureRect() const;
+
+		sf::FloatRect getLocalBounds() const;
+
+		sf::FloatRect getGlobalBounds() const;
+
+		void setTexture(const sf::Texture* texture);
+
+		void setTextureRect(const sf::IntRect& texture_rect);
+
+		void setTextureRect(int x, int y, int width, int height);
+
+		virtual ~EntitySprite() noexcept;
 
 	private:
 
-		Entity* _entity;
+		void updatePositions();
+
+		void updateTexCoords();
+
+		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+	private:
+
+		std::array<sf::Vertex, 4> _vertices;   // The vertices of the sprite.
+		const sf::Texture* _texture;		   // The texture of the sprite.
+		sf::IntRect _texture_rect;			   // The sub-rectangle of the texture to display.
+		Entity* _entity;					   // The entity that the sprite is representing.
+		std::array<Animation*, 4> _animations; // The animations of the sprite.
 	};
 }
 
--- src/GUI/EntitySprite.cpp
+++ src/GUI/EntitySprite.cpp
@@ -1,19 +1,92 @@
 #include <GUI/EntitySprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
 #include <Entity.hpp>
 
 gui::EntitySprite::EntitySprite(Entity* entity)
-	: sf::Sprite(), _entity(entity)
-{
-	if (_entity)
-	{
-		int x = _entity->getPosition().getX();
-		int y = _entity->getPosition().getY();
-		setPosition(x * 75, y * 40);
-	}
-}
+	: _vertices(), _texture(nullptr), _texture_rect(), _entity(entity), _animations() {}
 
 const Entity* gui::EntitySprite::getEntity() const
 {
 	return _entity;
+}
+
+Entity* gui::EntitySprite::getEntity()
+{
+	return _entity;
+}
+
+const sf::Texture* gui::EntitySprite::getTexture() const
+{
+	return _texture;
+}
+
+const sf::IntRect& gui::EntitySprite::getTextureRect() const
+{
+	return _texture_rect;
+}
+
+sf::FloatRect gui::EntitySprite::getLocalBounds() const
+{
+	return sf::FloatRect(0.f, 0.f, static_cast<float>(std::abs(_texture_rect.width)), static_cast<float>(std::abs(_texture_rect.height)));
+}
+
+sf::FloatRect gui::EntitySprite::getGlobalBounds() const
+{
+	return getTransform().transformRect(getLocalBounds());
+}
+
+void gui::EntitySprite::setTexture(const sf::Texture* texture)
+{
+	if (_texture != texture)
+	{
+		setTextureRect(sf::IntRect(0, 0, texture->getSize().x, texture->getSize().y));
+		_texture = texture;
+	}
+}
+
+void gui::EntitySprite::setTextureRect(const sf::IntRect& texture_rect)
+{
+	if (_texture_rect != texture_rect)
+	{
+		_texture_rect = texture_rect;
+		updatePositions();
+		updateTexCoords();
+	}
+}
+
+void gui::EntitySprite::setTextureRect(int x, int y, int width, int height)
+{
+	setTextureRect(sf::IntRect(x, y, width, height));
+}
+
+void gui::EntitySprite::updatePositions()
+{
+	_vertices[0].position = sf::Vector2f(0.f, 0.f);
+	_vertices[1].position = sf::Vector2f(0.f, getLocalBounds().height);
+	_vertices[2].position = sf::Vector2f(getLocalBounds().width, 0.f);
+	_vertices[3].position = sf::Vector2f(getLocalBounds().width, getLocalBounds().height);
+}
+
+void gui::EntitySprite::updateTexCoords()
+{
+	_vertices[0].texCoords = sf::Vector2f(_texture_rect.left, _texture_rect.top);
+	_vertices[1].texCoords = sf::Vector2f(_texture_rect.left, _texture_rect.top + _texture_rect.height);
+	_vertices[2].texCoords = sf::Vector2f(_texture_rect.left + _texture_rect.width, _texture_rect.top);
+	_vertices[3].texCoords = sf::Vector2f(_texture_rect.left + _texture_rect.width, _texture_rect.top + _texture_rect.height);
+}
+
+gui::EntitySprite::~EntitySprite() noexcept
+{
+	delete _entity;
+}
+
+void gui::EntitySprite::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	if (_texture)
+	{
+		states.transform *= getTransform();
+		states.texture = _texture;
+		target.draw(_vertices.data(), 4, sf::TriangleStrip, states);
+	}
 }

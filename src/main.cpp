0e68b6f602bd12d848c12396d624da68041ebb89
HoussamCzn <105058712+HoussamCzn@users.noreply.github.com>
2023-01-07 17:47:40 +0100

    add test main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,30 @@
+#include <utils/TextureManager.hpp>
+#include <utils/AnimationManager.hpp>
 #include <utils/SpriteManager.hpp>
-#include <components/Position.hpp>
-#include <components/Sprite.hpp>
-#include <utils/EntityCreator.hpp>
 #include <SFML/Graphics.hpp>
-#include <components/Sprite.hpp>
-#include <components/Position.hpp>
-#include <components/characters/Knight.hpp>
-#include <entt/entity/registry.hpp>
-#include <iostream>
 
-int main(int argc, char* argv[])
+int main()
 {
-	entt::registry world;
-	SpriteManager sm;
-	sm.load();
-	EntityCreator ec(world);
-	Position position(0, 0);
-	Sprite sprite(sm.getSprite('B', "knight"));
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!", sf::Style::Titlebar | sf::Style::Close);
+	window.setVerticalSyncEnabled(true);
+	SpriteManager sprite_manager;
+	sprite_manager.load();
 
-	auto entity = world.create();
-	ec.emplace<Position>(entity, position);
-	ec.emplace<Sprite>(entity, sprite);
+	SpriteComponent sprite = sprite_manager.get('B', "warrior");
 
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+	
 	return 0;
 }

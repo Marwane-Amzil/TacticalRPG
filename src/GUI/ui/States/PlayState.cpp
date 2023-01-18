dc830d7190d2e61908fb46d85a45791c978c31a8
thomas <thomasipod59122@gmail.com>
2023-01-18 13:28:57 +0100

    display characters
    display update to let you see the characters to select

--- src/GUI/ui/States/PlayState.cpp
+++ src/GUI/ui/States/PlayState.cpp
@@ -87,20 +87,71 @@ void PlayState::draw()
 	t.loadFromFile("assets/images/background.png", sf::IntRect(0,0, static_cast<int>(background_width/20), static_cast<int>(background_height/20)));
 	sf::Sprite s;
 	s.setTexture(t);
-	
+
+	int basic_x_pos = static_cast<int>(0.24 * x);
+	int basic_y_pos = static_cast<int>(0.04 * y);
 
 	for (int i = 0; i < 20; ++i) {
 		for (int j = 0; j < 20; ++j) {
-			s.setPosition(i* static_cast<int>(background_width / 20) + static_cast<int>(0.24*x), j * static_cast<int>(background_height / 20) + static_cast<int>(0.04 * y));
+			s.setPosition(i* static_cast<int>(background_width / 20) + basic_x_pos, 
+						  j * static_cast<int>(background_height / 20) + basic_y_pos);
+
 			_window.draw(s);
-			
 		}
 	}
-	_window.display();
-
-
 	
+	std::string color = "blue";
+
+	//for (int i = 0; i < 2; ++i) {
+		sf::Texture tknight;
+		tknight.loadFromFile("assets/images/characters/" + color + "/knight.png", sf::IntRect());
+		sf::Sprite sknight;
+		sknight.setTexture(tknight);
+		sknight.setPosition(static_cast<int>(0.83*x), 0.0);
+		sknight.setTextureRect(sf::IntRect(70, 650, 50, 50));
+		sknight.setScale(3.84, 3.84);
+		_window.draw(sknight);
+
+		std::cout << "assets/images/characters/" + color + "/knight.png";
+
+		sf::Texture tarcher;
+		tarcher.loadFromFile("assets/images/characters/" + color + "/archer.png", sf::IntRect());
+		sf::Sprite sarcher;
+		sarcher.setTexture(tarcher);
+		sarcher.setPosition(static_cast<int>(0.83 * x), static_cast<int>(0.2 * y));
+		sarcher.setTextureRect(sf::IntRect(70, 650, 50, 50));
+		sarcher.setScale(3.84, 3.84);
+		_window.draw(sarcher);
+
+		sf::Texture thealer;
+		thealer.loadFromFile("assets/images/characters/" + color + "/healer.png", sf::IntRect());
+		sf::Sprite shealer;
+		shealer.setTexture(thealer);
+		shealer.setPosition(static_cast<int>(0.83 * x), static_cast<int>(0.4 * y));
+		shealer.setTextureRect(sf::IntRect(70, 650, 50, 50));
+		shealer.setScale(3.84, 3.84);
+		_window.draw(shealer);
 
 
+		sf::Texture tmage;
+		tmage.loadFromFile("assets/images/characters/" + color + "/mage.png", sf::IntRect());
+		sf::Sprite smage;
+		smage.setTexture(tmage);
+		smage.setPosition(static_cast<int>(0.83 * x), static_cast<int>(0.6 * y));
+		smage.setTextureRect(sf::IntRect(70, 650, 50, 50));
+		smage.setScale(3.84, 3.84);
+		_window.draw(smage);
 
+		sf::Texture twarrior;
+		twarrior.loadFromFile("assets/images/characters/" + color + "/warrior.png", sf::IntRect());
+		sf::Sprite swarrior;
+		swarrior.setTexture(twarrior);
+		swarrior.setPosition(static_cast<int>(0.83 * x), static_cast<int>(0.8 * y));
+		swarrior.setTextureRect(sf::IntRect(70,650, 50, 50));
+		swarrior.setScale(3.84,3.84);
+		_window.draw(swarrior);
+
+	//	color = "red";
+	//}
+		_window.display();
 }

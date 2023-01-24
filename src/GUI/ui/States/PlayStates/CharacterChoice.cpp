bbd11e35e62a39c8c76f11f93f9608a2db22a243
Amzil Marwane <63516970+sardinefroidedu4@users.noreply.github.com>
2023-01-24 22:45:52 +0100

    Advancements in the CharacterChoice Class

--- src/GUI/ui/States/PlayStates/CharacterChoice.cpp
+++ src/GUI/ui/States/PlayStates/CharacterChoice.cpp
@@ -18,20 +18,19 @@ CharacterChoice::CharacterChoice(StateMachine& machine, sf::RenderWindow& window
 
 	for (size_t i = 0; i < 2; i++){
 		for (auto characterName : _m_characterNames){
-			
 			m_characters[nb].setTexture(_textureManager.getTextureAt(color, characterName));
 			m_characters[nb].setPosition(multyx * x,  multiy * y);
 			m_characters[nb].setTextureRect(sf::IntRect(70, 650, 50, 50));
-			m_characters[nb].setScale(2.8, 2.8);
+			m_characters[nb].setScale(2.8f, 2.8f);
 			
-			multiy = multiy - 0.2;
+			multiy = multiy - 0.2f;
 			if (multiy < 0) {
-				multiy = 0.8;
+				multiy = 0.8f;
 			}
 			nb++;
 			if (nb == 5){
 				color = 'R';
-				multyx = 0.83;
+				multyx = 0.83f;
 			}
 		}
 	}
@@ -50,15 +49,36 @@ void CharacterChoice::resume()
 
 void CharacterChoice::update()
 {
-	/*for (sf::Event event = sf::Event(); _window.pollEvent(event);)
+	sf::Event event;
+	while (_window.pollEvent(event))
 	{
+		if (event.type == sf::Event::Closed)
+			_window.close();
+		if (event.type == sf::Event::KeyPressed)
+		{
+			if (event.key.code == sf::Keyboard::Escape)
+				_window.close();
+		}
+	}
+	
 
-	}
-	for (auto character : m_characters)
+
+	for (size_t i = 0; i < m_characters.size(); i++)
 	{
-		character.update(_clock.getElapsedTime());
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			if (m_characters[i].getGlobalBounds().contains(sf::Mouse::getPosition(_window).x, sf::Mouse::getPosition(_window).y))
+			{
+				int pos_grille_x = static_cast<int>(((sf::Mouse::getPosition(_window).x)-460) / 50) * 50 + 460;
+				int pos_grille_y = static_cast<int>(((sf::Mouse::getPosition(_window).y)-40) / 50) * 50 + 40;
+				auto [x, y] = _window.getSize();
+				std::cout << m_characters[i].getPosition().x << " " << m_characters[i].getPosition().y << std::endl;
+				m_characters[i].setScale(1, 1);
+				m_characters[i].setPosition(pos_grille_x,pos_grille_y);
+
+			}
+		}
 	}
-	*/
 }
 
 void CharacterChoice::draw()
